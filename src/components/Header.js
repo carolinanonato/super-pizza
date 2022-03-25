406fbd80edc17e89797ab6a98525151dc8933df4
Carolina Nonato <csnonato@gmail.com>
2022-03-24 21:38:45 -0400

    router and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Header() {
+
+
+function Header() {
     return (
-        <div>Header</div>
+        <nav className="d-flex p-2" >
+            <h1>SUPER PIZZA</h1>
+            <Link to="/">Home</Link> | {" "}
+            <Link to="/menu" > Menu</Link > | {" "}
+            <Link to="/contact" > Contact</Link >
+
+
+        </nav >
     )
 }
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import Home from "./pages/Home";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/menu" element={<Menu />} />
+
+    </Routes>
+    <Hero />
+    <Footer />
+  </Router >,
+
+  document.getElementById("root")
 );
