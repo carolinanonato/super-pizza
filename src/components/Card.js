33c2ec1530aa6930270ea081df98258f626e9344
Carolina Nonato <csnonato@gmail.com>
2022-03-26 10:21:50 -0400

    menu items

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,36 @@ import React from 'react'
 import "../index.css";
 
 export default function Card(props) {
+
+    const [count, setCount] = React.useState(0)
+    function add() {
+        setCount(count + 1)
+    }
+
+    function subtract() {
+        setCount(count - 1)
+    }
     return (
 
         <div className="card">
-            <img className="card-img" src={props.img} alt="" />
-            <div className="info">
-                <h1 className="title">{props.title}</h1>
-                <p className="ingredients">{props.ingredients}</p>
-                <span className="price">{props.price}</span>
+            <div className="first-card">
+                <img className="card-img" src={props.img} alt="" />
             </div>
+            <div className="second-card">
+                <div className="info">
+                    <h1 className="card-title">{props.title}</h1>
+                    <p className="ingredients">{props.ingredients}</p>
+                    <span className="price">${props.price}</span>
+                </div>
 
+            </div>
+            <div className="counter">
+                <button className="counter--minus" onClick={subtract}>–</button>
+                <div className="counter--count">
+                    <h1>{count}</h1>
+                </div>
+                <button className="counter--plus" onClick={add}>+</button>
+            </div>
 
         </div >
 
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -5,21 +5,23 @@ import data from "../data"
 export default function Menu() {
     const cards = data.map(item => {
         return (
-            <>
 
-                <Card
-                    key={item.id}
-                    img={item.img}
-                    title={item.title}
-                    ingredients={item.ingredients}
-                    price={item.price}
+            <Card
+                key={item.id}
+                img={item.img}
+                title={item.title}
+                ingredients={item.ingredients}
+                price={item.price}
 
-                />
-            </>
+            />
+
         )
     })
 
     return (
-        <div className="cards-list">{cards}</div>
+        <>
+            <h1 class="menu-text">Menu</h1>
+            <div className="cards-list">{cards}</div>
+        </>
     )
 }
